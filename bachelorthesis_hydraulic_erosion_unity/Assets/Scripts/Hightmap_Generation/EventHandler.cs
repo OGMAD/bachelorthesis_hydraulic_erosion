b9d05c43df91714546f8b96914c97bc9ea41b60c
Maximilian Alexander Deichsel <deichsel1@live.de>
2022-08-05 20:21:09 +0200

    data gets loaded in editor

--- bachelorthesis_hydraulic_erosion_unity/Assets/Scripts/Hightmap_Generation/EventHandler.cs
+++ bachelorthesis_hydraulic_erosion_unity/Assets/Scripts/Hightmap_Generation/EventHandler.cs
@@ -9,16 +9,23 @@ public class EventHandler : MonoBehaviour
     public GameObject CloudmapItem_prefab;
     private List<CloudmapItem> CloudmapItem_List = new List<CloudmapItem>();
 
+    #region Variables
     public string PName { get; set; }
+    public InputField GName;
     
     public string PXwidth { get; set; }
+    public InputField GXwidth;
     public string PYheight { get; set; }
-
+    public InputField GYheight;
     public string POffsetX { get; set; }
+    public InputField GOffsetX;
     public string POffsetY { get; set; }
+    public InputField GOffsertY;
 
     public string PStrength { get; set; }
+    public InputField GStrength;
     public string PScale { get; set; }
+    public InputField GScale;
 
 
     private string Name { get; set; }
@@ -30,6 +37,7 @@ public class EventHandler : MonoBehaviour
 
     private float Strength { get; set; }
     private float Scale { get; set; }
+    #endregion
 
     private void CastAll()
     {
@@ -74,8 +82,46 @@ public class EventHandler : MonoBehaviour
             else
             {
                 CloudmapItem_List[x].IsActive = true;
+                SetValuesInEdit(CloudmapItem_List[x]);
             }
         }
+    }
+
+    private void SetValuesInEdit(CloudmapItem item)
+    {
+        GName.text = item.Name;
+        GXwidth.text = item.Xwidth.ToString();
+        GYheight.text = item.Yheight.ToString();
+        GOffsetX.text = item.OffsetX.ToString();
+        GOffsertY.text = item.OffsetY.ToString();
+        GStrength.text = item.Strength.ToString();
+        GScale.text = item.Scale.ToString();
+        Debug.Log("SetValuesInEdit");
+    }
+
+    private CloudmapItem GetActiveCloudmapItem()
+    {
+        for (int x = 0; x < CloudmapItem_List.Count; x++)
+        {
+            if (CloudmapItem_List[x].IsActive)
+            {
+                return CloudmapItem_List[x];
+            }
+        }
+        return null;
+    }
+
+    public void GeneratePerlinNoiseMap()
+    {
+        CastAll();
+        CloudmapItem item = GetActiveCloudmapItem();
+        item.Name = Name;
+        item.Xwidth = Xwidth;
+        item.Yheight = Yheight;
+        item.OffsetX = OffsetX;
+        item.OffsetY = OffsetY;
+        item.Strength = Strength;
+        item.Scale = Scale;
     }
 }
 
@@ -93,4 +139,16 @@ public class CloudmapItem
     public float Scale { get; set; }
 
     public bool IsActive { get; set; }
+
+    public CloudmapItem()
+    {
+        Name = "SomeHightmap";
+        Xwidth = 255;
+        Yheight = 255;
+        OffsetX = 0;
+        OffsetY = 0;
+        Strength = 1;
+        Scale = 4;
+        IsActive = false;
+    }
 }
