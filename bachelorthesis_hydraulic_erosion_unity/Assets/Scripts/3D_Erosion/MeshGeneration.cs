9d678409c1e7da260e5abed5efc75440fc37d9d6
OGMAD <58218184+OGMAD@users.noreply.github.com>
2022-09-08 10:27:01 +0200

    works better but still buggy as hell

--- bachelorthesis_hydraulic_erosion_unity/Assets/Scripts/3D_Erosion/MeshGeneration.cs
+++ bachelorthesis_hydraulic_erosion_unity/Assets/Scripts/3D_Erosion/MeshGeneration.cs
@@ -11,6 +11,7 @@ public static class MeshGeneration
         Mesh mesh = new Mesh();
 
         AddVerticesToMesh(vertices, mesh);
+        CalculateAndAddUVToMesh(vertices, mesh);
         CalculateTriangles(vertices, mesh);
 
         meshfilter.mesh = mesh;
@@ -21,55 +22,69 @@ public static class MeshGeneration
     private static void AddVerticesToMesh(Vertex[,] vertices, Mesh mesh)
     {
         Vector3[] verts = new Vector3[vertices.GetUpperBound(0) * vertices.GetUpperBound(1)];
+        int counter = 0;
         for(int columnIndex = 0; columnIndex < vertices.GetUpperBound(0); columnIndex++)
         {
             for (int rowIndex = 0; rowIndex < vertices.GetUpperBound(1); rowIndex++)
             {
-                verts[columnIndex] = new Vector3
+                verts[counter] = new Vector3
                     (
                         vertices[columnIndex, rowIndex].XCoord, 
                         vertices[columnIndex, rowIndex].YCoord, 
                         vertices[columnIndex, rowIndex].ZCoord
                     );
+                counter++;
             }
         }
+        
         mesh.vertices = verts;
+    }
 
+    private static void CalculateAndAddUVToMesh(Vertex[,] vertices, Mesh mesh)
+    {
         Vector2[] vertsuv = new Vector2[vertices.GetUpperBound(0) * vertices.GetUpperBound(1)];
         int counter = 0;
         for (int columnIndex = 0; columnIndex < vertices.GetUpperBound(0); columnIndex++)
         {
             for (int rowIndex = 0; rowIndex < vertices.GetUpperBound(1); rowIndex++)
             {
-                vertsuv[counter] = new Vector2(vertices[columnIndex, rowIndex].XCoord, vertices[columnIndex, rowIndex].ZCoord);
+                vertsuv[counter] = new Vector2
+                    (
+                        vertices[columnIndex, rowIndex].XCoord, 
+                        vertices[columnIndex, rowIndex].ZCoord
+                    );
                 counter++;
             }
         }
-    }
 
+        mesh.uv = vertsuv;
+    }
     private static void CalculateTriangles(Vertex[,] vertices, Mesh mesh) 
     {
-        int[] tris = new int[((vertices.GetUpperBound(0) * vertices.GetUpperBound(1)) - vertices.GetUpperBound(0)) * 3 ];
+        int[] tris = new int[((vertices.GetUpperBound(0) * vertices.GetUpperBound(1)) - vertices.GetUpperBound(1)) * 3 ];
         int[] singletris = new int[3];
         int counter = 0;
-        for(int i = 0; i < mesh.vertices.Length - vertices.GetUpperBound(0); i++)
+        for(int i = 0; i < mesh.vertices.Length - vertices.GetUpperBound(1); i++)
         {
-            if (i % 2 == 0)
+            if (i % vertices.GetUpperBound(1) != 0)
             {
-                singletris = RightSiteTriangle(i, vertices.GetUpperBound(0));
-            }
-            else
-            {
-                singletris =LeftSiteTriangle(i,vertices.GetUpperBound(0));
-            }
+                if (i % 2 == 0)
+                {
+                    singletris = RightSiteTriangle(i, vertices.GetUpperBound(1));
+                }
+                else
+                {
+                    singletris = LeftSiteTriangle(i, vertices.GetUpperBound(1));
+                }
 
-            for(int j = 0; j < singletris.Length; j++)
-            {
-                tris[counter] = singletris[j];
-                counter++;
+                for (int j = 0; j < singletris.Length; j++)
+                {
+                    tris[counter] = singletris[j];
+                    counter++;
+                }
             }
         }
-
+        
         mesh.triangles = tris;
     }
 
